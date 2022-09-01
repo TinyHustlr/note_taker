09318cde96c11bfcd0ab817658cd00a3cfd2500e
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-09-01 14:37:05 +1000

    Note taking working
    Notes being added to list and note box clearing

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const newNoteEl = document.querySelector("#newnote-el")
 const noteBtn = document.querySelector("#note-btn")
+let notesTakenEl = document.querySelector("#notestaken-el")
+let noteStore = []
 
-noteBtn.addEventListener("click", function() {
-    alert("Button pressed")
+noteBtn.addEventListener("click", function () {
+    saveNote ()
 })
+
+function saveNote() {
+    noteStore = []
+    noteStore.push(newNoteEl.value)
+    notesTakenEl.innerHTML += "<li>" + (noteStore) + "</li>"
+    newNoteEl.value = ""
+}
+
