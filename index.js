eb65a96502e12bb66ecf9227e60b56dbd47ae7fd
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-09-01 14:08:13 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const newNoteEl = document.querySelector("#newnote-el")
+const noteBtn = document.querySelector("#note-btn")
+
+noteBtn.addEventListener("click", function() {
+    alert("Button pressed")
+})
