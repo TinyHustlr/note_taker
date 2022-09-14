4aba3d641b33814767892e10e3d1453082ad112e
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-09-15 00:42:52 +1000

    Local storage working but not rendering out

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const noteBtn = document.querySelector("#note-btn")
+const deleteBtn = document.querySelector("#delete-btn")
 const noteslistEL = document.querySelector("#noteslist-el")
 let newNoteEl = document.getElementById("newnote-el")
 let notesTaken = []
 
 if (notesTaken) {
-    let renderNotes = JSON.parse(localStorage.getItem("ZeldasNotes")) 
-    noteslistEL.innerHTML += `<li class="listItem-el">${renderNotes}</li>`   
+    NotesTaken = localStorage.getItem("ZeldasNotes")
+    for (let i = 0; i < notesTaken.length; i++) {
+      noteslistEL.innerHTML += `<li class="listItem-el">${notesTaken[i]}</li>`    
+    }
 }
 
 noteBtn.addEventListener("click", function() {
     notesTaken += newNoteEl.value // add notes to the array
-    localStorage.setItem("ZeldasNotes", JSON.stringify(notesTaken)) // add notes to local storage
-    newNoteEl.textContent = ""
+    localStorage.setItem("ZeldasNotes", notesTaken) // add notes to local storage
+    console.log(notesTaken)
 })
 
+deleteBtn.addEventListener("click", function() {
+    alert("You are about to delete all your notes!")
+    alert("Are you sure? This cannot be undone.")
+    localStorage.clear()
+    notesTaken = []
+})
