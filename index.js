acddd4e4edcb1bed2d7ec6dc7aa1f4b6a093c086
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-09-15 00:52:59 +1000

    Zelda chat draft

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const noteBtn = document.querySelector("#note-btn")
 const deleteBtn = document.querySelector("#delete-btn")
 const noteslistEL = document.querySelector("#noteslist-el")
-let newNoteEl = document.getElementById("newnote-el")
+const zeldachatEl = document.querySelector("#zeldachat-el")
+let newNoteEl = document.querySelector("#newnote-el")
 let notesTaken = []
+let zeldaComments = ["Okay I got that. I'll be sure Ganon does not see it.", "Noted. I might also let Link know for safe keeping.", "Well that is very interesting. I shall remember it during my research of Hyrule.", "Thank you for trusting me with that very important information.", "Oh if Ganon knew what you just wrote, we could be in a lot of trouble"]
+
+
 
 if (notesTaken) {
     NotesTaken = localStorage.getItem("ZeldasNotes")
@@ -14,7 +18,8 @@ if (notesTaken) {
 noteBtn.addEventListener("click", function() {
     notesTaken += newNoteEl.value // add notes to the array
     localStorage.setItem("ZeldasNotes", notesTaken) // add notes to local storage
-    console.log(notesTaken)
+    let randomChatIndex = Math.floor(Math.random() * zeldaComments.length)
+    zeldachatEl.textContent = zeldaComments[randomChatIndex]
 })
 
 deleteBtn.addEventListener("click", function() {
