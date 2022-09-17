89bf74c7d5301f8e92bf7dd3496a7f03e7a74ad1
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-09-17 23:12:02 +1000

    Note taker working v1

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const noteBtn = document.querySelector("#note-btn")
 const deleteBtn = document.querySelector("#delete-btn")
 const noteslistEL = document.querySelector("#noteslist-el")
 const zeldachatEl = document.querySelector("#zeldachat-el")
+const dellastNoteEl  = document.querySelector("#dellastnote-btn")
+console.log(dellastNoteEl)
 let newNoteEl = document.querySelector("#newnote-el")
 let notesTaken = []
 let zeldaComments = ["Okay I got that. I'll be sure Ganon does not see it.", "Noted. I might also let Link know for safe keeping.", "Well that is very interesting. I shall remember it during my research of Hyrule.", "Thank you for trusting me with that very important information.", "Oh if Ganon knew what you just wrote, we could be in a lot of trouble"]
@@ -21,20 +23,27 @@ function renderNotes() {
     noteslistEL.innerHTML = notesItems 
 }  
 
-noteBtn.addEventListener("click", function() {
+noteBtn.addEventListener("click", function () {
     notesTaken.push(newNoteEl.value) // add notes to the array
-    newNoteEl = ""
+    newNoteEl.value = ""
     localStorage.setItem("ZeldasNotes", JSON.stringify(notesTaken)) // add notes to local storage
     let randomChatIndex = Math.floor(Math.random() * zeldaComments.length)
     zeldachatEl.textContent = zeldaComments[randomChatIndex]
     renderNotes()
 })
 
+dellastNoteEl.addEventListener("click", function() {
+    notesTaken.pop() // remove last note from array
+    localStorage.setItem("ZeldasNotes", JSON.stringify(notesTaken)) // add notes to local storage
+    renderNotes()
+})
+
+
 deleteBtn.addEventListener("click", function() {
     alert("You are about to delete all your notes!")
     alert("Are you sure? This cannot be undone.")
     localStorage.clear()
-    newNoteEl = ""
+    notesTaken = []
     renderNotes()
 })
 
